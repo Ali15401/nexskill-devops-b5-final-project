9bc2602cbb88e1ceae1262156b76a02962b1b17b
Asad Ullah <asadation@gmail.com>
2025-12-15 17:03:13 +0500

    Tested and updated codebase

--- analytics-service/config.js
+++ analytics-service/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || '8.222.170.22',
         port: process.env.DB_PORT || 5432,
         database: process.env.DB_NAME || 'urlshortener',
         user: process.env.DB_USER || 'postgres',
