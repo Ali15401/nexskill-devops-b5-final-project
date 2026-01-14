eaf42638b401735867f3f0250391bb4677890b08
Ali Rehman <alirehman15401@gmail.com>
2026-01-14 21:40:59 +0500

    Refactor config to use os.getenv with defaults
    Updated environment variable retrieval to use os.getenv with default values for better clarity.

--- link-service/config.py
+++ link-service/config.py
@@ -1,22 +1,14 @@
 import os
 
+# configure these 4 values from repository secret. If you want to run project on localhost then put values here.
 class Config:
-    """
-    This configuration class reads settings from the environment variables
-    provided by the ECS Task Definition.
-    """
-    # Read the database connection details
-    DATABASE_HOST = os.environ.get('DB_HOST')
-    DATABASE_PORT = os.environ.get('DB_PORT', 5432) # Default to 5432 if not provided
-    DATABASE_NAME = os.environ.get('DB_NAME', 'projectdb')
-    DATABASE_USER = os.environ.get('DB_USER', 'projectadmin')
-
-    # This is a placeholder for now. The service will default to a dummy URL.
-    ANALYTICS_SERVICE_URL = os.environ.get('ANALYTICS_SERVICE_URL', 'http://project-alb-924486123.us-east-1.elb.amazonaws.com')
-
-    # These are needed for the app to fetch the password from AWS Secrets Manager
-    DB_SECRET_ARN = os.environ.get('DB_SECRET_ARN')
-    AWS_REGION = os.environ.get('AWS_REGION')
-
-    # This is the port your application will run on, read from the environment
-    PORT = os.environ.get("PORT", 5001)
+    DATABASE_HOST = os.getenv('DB_HOST', '<DB_HOST>') 
+    DATABASE_PORT = os.getenv('DB_PORT', '5432')
+    DATABASE_NAME = os.getenv('DB_NAME', '<DB_NAME>')
+    DATABASE_USER = os.getenv('DB_USER', '<DB_USER>')
+    DATABASE_PASSWORD = os.getenv('DB_PASSWORD', '<DB_PASSWORD>')
+    ANALYTICS_SERVICE_URL = os.getenv(
+        "ANALYTICS_SERVICE_URL",
+        "http://https://project-alb-924486123.us-east-1.elb.amazonaws.com//api/analytics"
+    )
+    PORT = int(os.getenv('PORT', 3000))
