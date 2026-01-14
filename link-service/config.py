7123348ab11bf1b840e4fca40271d5e13c8fa7c8
Ali Rehman <alirehman15401@gmail.com>
2026-01-14 16:09:20 +0000

    Update config.py

--- link-service/config.py
+++ link-service/config.py
@@ -1,16 +1,22 @@
 import os
 
 class Config:
-    # Read the database credentials from the environment variables provided by ECS
+    """
+    This configuration class reads settings from the environment variables
+    provided by the ECS Task Definition.
+    """
+    # Read the database connection details
     DATABASE_HOST = os.environ.get('DB_HOST')
-    DATABASE_PORT = os.environ.get('DB_PORT', 5432) # Default to 5432 if not set
+    DATABASE_PORT = os.environ.get('DB_PORT', 5432) # Default to 5432 if not provided
     DATABASE_NAME = os.environ.get('DB_NAME', 'projectdb')
     DATABASE_USER = os.environ.get('DB_USER', 'projectadmin')
 
-    # This is a placeholder. For now, we will not deploy the analytics service.
-    ANALYTICS_SERVICE_URL = os.environ.get('ANALYTICS_SERVICE_URL', 'http://localhost:9999') # Default to a dummy URL
+    # This is a placeholder for now. The service will default to a dummy URL.
+    ANALYTICS_SERVICE_URL = os.environ.get('ANALYTICS_SERVICE_URL', 'http://localhost:9999')
 
-    # Note: The password will be handled directly in the app.py to fetch from Secrets Manager
-    # We just need to make sure the app knows where to look for the secret ARN.
+    # These are needed for the app to fetch the password from AWS Secrets Manager
     DB_SECRET_ARN = os.environ.get('DB_SECRET_ARN')
     AWS_REGION = os.environ.get('AWS_REGION')
+
+    # This is the port your application will run on, read from the environment
+    PORT = os.environ.get("PORT", 5001)
